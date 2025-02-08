89b08eda4807e36ea00aee734d4731709f590661
Parmod Kumar <parmod1feb2002@gmail.com>
2025-02-08 16:56:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -50,9 +50,9 @@ app.use((err, req, res, next) => {
 });
 
 // ✅ Export for Vercel (No `app.listen()`)
-// module.exports = app;
+module.exports = app;
 
-app.listen(3000,(req,res)=>{
-    console.log(`server is listing on 3000`);
-})
+// app.listen(3000,(req,res)=>{
+//     console.log(`server is listing on 3000`);
+// })
 
